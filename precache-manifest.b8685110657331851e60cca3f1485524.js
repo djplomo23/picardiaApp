dfd4e2024ff4432ce3a57039508ac614af2de757
djplomo23 <69323758+djplomo23@users.noreply.github.com>
2020-09-23 22:44:14 -0400

    Updates

---
+++ precache-manifest.b8685110657331851e60cca3f1485524.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6316352b49abaa085733f4eee24b06a",
+    "url": "/picardiaApp/index.html"
+  },
+  {
+    "revision": "8fd03e3b7476d29157a6",
+    "url": "/picardiaApp/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "08c97658a0b7b8638030",
+    "url": "/picardiaApp/static/css/main.49fdbdf8.chunk.css"
+  },
+  {
+    "revision": "8fd03e3b7476d29157a6",
+    "url": "/picardiaApp/static/js/2.ccd0789c.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/picardiaApp/static/js/2.ccd0789c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c97658a0b7b8638030",
+    "url": "/picardiaApp/static/js/main.45b5e0e6.chunk.js"
+  },
+  {
+    "revision": "fed6deb75b62dc41c55c",
+    "url": "/picardiaApp/static/js/runtime-main.13d14012.js"
+  },
+  {
+    "revision": "c782774f2084ca9e10d9847140619a05",
+    "url": "/picardiaApp/static/media/LOGO-PICARDIA--BLANCO.c782774f.png"
+  },
+  {
+    "revision": "d4fd60af5a1836ef835fe9cf10d3049b",
+    "url": "/picardiaApp/static/media/LOGO-PICARDIA-NEGRO.d4fd60af.png"
+  },
+  {
+    "revision": "0446df373cc7501fa9397232f855addf",
+    "url": "/picardiaApp/static/media/LOGO-PICARDIA-Silueta.0446df37.png"
+  },
+  {
+    "revision": "1735c3c6aa36108634f0414f6a5e27a6",
+    "url": "/picardiaApp/static/media/Offer1.1735c3c6.png"
+  },
+  {
+    "revision": "da0a133b40ea9cf1c89d4841362ebff8",
+    "url": "/picardiaApp/static/media/esposas.da0a133b.png"
+  },
+  {
+    "revision": "5e2b96cd8e08d67b6ebe73b27ea8fb64",
+    "url": "/picardiaApp/static/media/f1.5e2b96cd.png"
+  },
+  {
+    "revision": "903b30718d20f82066e79739b88a0c6f",
+    "url": "/picardiaApp/static/media/f2.903b3071.jpg"
+  },
+  {
+    "revision": "4c7c8c2cfa26dc8fb7a3302fd1aa9fbb",
+    "url": "/picardiaApp/static/media/f3.4c7c8c2c.jpg"
+  },
+  {
+    "revision": "61bd2cdfdaea2d83eedfcafa31e637e5",
+    "url": "/picardiaApp/static/media/rostro.61bd2cdf.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1fb5717c6cd804ee1418d26fdb32209.js"
+  "/picardiaApp/precache-manifest.b8685110657331851e60cca3f1485524.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/picardiaApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
